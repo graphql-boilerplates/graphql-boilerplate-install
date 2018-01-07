78e4d6ee83f76118a876f88312e5d51ac451c422
Kim Brandwijk <kim.brandwijk@gmail.com>
2018-01-07 16:07:42 +0100

    fix: added getInfo method

--- src/graphcool.ts
+++ src/graphcool.ts
@@ -13,6 +13,15 @@ export async function deploy(silent: boolean) {
     .catch(err => console.error(err))
 }
 
+export async function getInfo(): Promise<any> {
+  return spawn('graphcool', ['info', '--current', '--json'], { stdio: 'pipe'})
+  .then(res => {
+    const endpointInfo = JSON.parse(res)
+    return endpointInfo
+  })
+  .catch(err => console.error(err))
+}
+
 export async function writeEnv() {
   return spawn('graphcool', ['info', '--current', '--json'], { stdio: 'pipe'})
   .then(res => {
